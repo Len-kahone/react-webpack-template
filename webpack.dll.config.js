027bede20a1fdaca63be7756afd74cd97d0aa58c
Len-kahone <1030462746@qq.com>
2020-08-01 12:54:52 +0800

    完整的webpack脚手架

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,5 +1,6 @@
 const path=require("path");
 const webpack=require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports={
     mode:"production",
     entry:{
@@ -8,13 +9,16 @@ module.exports={
     output:{
         filename:"[name].dll.js",
         path:path.resolve(__dirname,"dll"),
+        //打包的库向外暴露的内容叫什么名字
         library:"[name]"
 
     },
     plugins:[
+        new CleanWebpackPlugin(), //打包前先清空文件夹
+        //打包生成一个mainfest.json文件，提供jq的映射
         new webpack.DllPlugin({
-            name:"[name]",
-            path:path.resolve(__dirname,"dll/[name].mainfest.json")
+            name:"[name]",//映射库暴露的内容名称
+            path:path.resolve(__dirname,"dll/mainfest.json")//输出文件路径
         })
     ]
 };
--- webpack.pro.config.js
+++ webpack.pro.config.js
@@ -2,9 +2,10 @@ const baseConfig = require("./webpack.base.config.js");
 const { merge } = require("webpack-merge");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const webpack=require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path=require("path");
+const addAssetHtmlWebpackPlugin= require("add-asset-html-webpack-plugin");
 module.exports = merge(baseConfig, {
   mode: "production",
   output: {
@@ -27,9 +28,20 @@ module.exports = merge(baseConfig, {
   plugins: [
     new CleanWebpackPlugin(), //打包前先清空文件夹
     new OptimizeCSSAssetsPlugin({}), //对css进行压缩
+    //将css从js中分离出来，以link的方式引用
     new MiniCssExtractPlugin({
       filename: "css/[name].[contenthash:6].css",
-    }), //将css从js中分离出来，以link的方式引用
+    }), 
+    //告诉webpack哪些库不参与打包，同时使用时名称也得变
+    new webpack.DllReferencePlugin({
+      manifest:require(path.resolve(__dirname,"dll/mainfest.json")),
+    }),
+    //将某个文件打包输出去，并在html中自动引入该资源
+    new addAssetHtmlWebpackPlugin([
+      {
+        filepath:path.resolve(__dirname,"dll/jquery.dll.js")
+      }
+    ])
   ],
   externals: {
     //不打包jq，通过script标签引入
